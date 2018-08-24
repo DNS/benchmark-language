9453b4659671518501b8d0ca98ee6e1297bf37ab
dns <sirait10@gmail.com>
2018-08-24 17:46:22 +0700

    Fixed locale bug. Decimal symbol , support added.

--- timecmd.bat
+++ timecmd.bat
@@ -7,7 +7,7 @@ set start=%time%
 cmd /c %*
 
 set end=%time%
-set options="tokens=1-4 delims=:."
+set options="tokens=1-4 delims=:.,"
 for /f %options% %%a in ("%start%") do set start_h=%%a&set /a start_m=100%%b %% 100&set /a start_s=100%%c %% 100&set /a start_ms=100%%d %% 100
 for /f %options% %%a in ("%end%") do set end_h=%%a&set /a end_m=100%%b %% 100&set /a end_s=100%%c %% 100&set /a end_ms=100%%d %% 100
 
